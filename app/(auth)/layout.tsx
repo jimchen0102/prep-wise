16af105ef717fe6f1301f03bb706bf6c165cbf7b
jimchen <azx011014@gmail.com>
2025-03-22 17:11:27 +0800

    feat: implement authentication layout and enhance global styling
    - Add `AuthLayout` component for the authentication layout.
    - Create a sign-in page using the `AuthForm` component.
    - Create a sign-up page using the `AuthForm` component.
    - Add `RootLayout` component for the root layout.
    - Rename the home page file and update display text from "Page" to "Home Page".
    - Add CSS theme variables for colors and fonts, and apply global styles for paragraphs, headings, and lists.
    - Define utility class for background pattern styling.
    
    Signed-off-by: jimchen <azx011014@gmail.com>

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
